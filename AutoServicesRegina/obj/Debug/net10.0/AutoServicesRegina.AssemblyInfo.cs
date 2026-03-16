75033d858e1d8c4c16f62672553f3b09dd62c649
Ihor Syniak <19970602irop@gmil.com>
2026-03-16 22:52:57 +0200

    Updated project information

--- AutoServicesRegina/obj/Debug/net10.0/AutoServicesRegina.AssemblyInfo.cs
+++ AutoServicesRegina/obj/Debug/net10.0/AutoServicesRegina.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoServicesRegina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db77b133f745a2c4ce1191a3be8693d2d3efbf9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7417fc89003c99e5e3ef98c9e2ff2160db161b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoServicesRegina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoServicesRegina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
